32cf2deb8e72e1e470220293026553eb6ad9a15b
Evan <esundwick@gannett.com>
2017-09-02 17:13:12 -0400

    re-initialized bot with botkit's starter kit. removed slash command support temporarily.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var debug = require('debug')('botkit:main');
 // var config = require('./config.json');
 
 var bot_options = {
-	clientID: process.env.SLACK_CLIENT_ID,
+	clientId: process.env.SLACK_CLIENT_ID,
 	clientSecret: process.env.SLACK_CLIENT_SECRET,
 	scopes: ['bot'],
 	studio_token: process.env.studio_token,
@@ -39,6 +39,75 @@ bot.api.team.info({}, (err, res) => {
     });
 });
 
+// Set up an Express-powered webserver to expose oauth and webhook endpoints
+var webserver = require(__dirname + '/components/express_webserver.js')(controller);
+
+// Set up a simple storage backend for keeping a record of customers
+// who sign up for the app via the oauth
+require(__dirname + '/components/user_registration.js')(controller);
+
+// Send an onboarding message when a new team joins
+require(__dirname + '/components/onboarding.js')(controller);
+
+// Load in some helpers that make running Botkit on Glitch.com better
+require(__dirname + '/components/plugin_glitch.js')(controller);
+
+// Enable Dashbot.io plugin
+require(__dirname + '/components/plugin_dashbot.js')(controller);
+
+
+var normalizedPath = require("path").join(__dirname, "skills");
+require("fs").readdirSync(normalizedPath).forEach(function(file) {
+  require("./skills/" + file)(controller);
+});
+
+
+
+// This captures and evaluates any message sent to the bot as a DM
+// or sent to the bot in the form "@bot message" and passes it to
+// Botkit Studio to evaluate for trigger words and patterns.
+// If a trigger is matched, the conversation will automatically fire!
+// You can tie into the execution of the script using the functions
+// controller.studio.before, controller.studio.after and controller.studio.validate
+if (process.env.studio_token) {
+    controller.on('direct_message,direct_mention,mention', function(bot, message) {
+        controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(function(convo) {
+            if (!convo) {
+                // no trigger was matched
+                // If you want your bot to respond to every message,
+                // define a 'fallback' script in Botkit Studio
+                // and uncomment the line below.
+                // controller.studio.run(bot, 'fallback', message.user, message.channel);
+            } else {
+                // set variables here that are needed for EVERY script
+                // use controller.studio.before('script') to set variables specific to a script
+                convo.setVar('current_time', new Date());
+            }
+        }).catch(function(err) {
+            bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
+            debug('Botkit Studio: ', err);
+        });
+    });
+} else {
+    console.log('~~~~~~~~~~');
+    console.log('NOTE: Botkit Studio functionality has not been enabled');
+    console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
+}
+
+
+
+
+function usage_tip() {
+    console.log('~~~~~~~~~~');
+    console.log('Botkit Starter Kit');
+    console.log('Execute your bot application like this:');
+    console.log('clientId=<MY SLACK CLIENT ID> clientSecret=<MY CLIENT SECRET> PORT=3000 studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
+    console.log('Get Slack app credentials here: https://api.slack.com/apps')
+    console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
+    console.log('~~~~~~~~~~');
+}
+
+/* maybe RTM is deprecated...
 bot.startRTM(function(err,bot,payload) {
   if (err) {
     throw new Error('Could not connect to Slack');
@@ -55,16 +124,15 @@ controller.setupWebserver(process.env.PORT || 3001, function(err, webserver) {
     });
 });
 
-/*
-  // close the RTM for the sake of it in 5 seconds
-  setTimeout(function() {
+// close the RTM for the sake of it in 5 seconds
+setTimeout(function() {
       bot.closeRTM();
   }, 5000);
 });*/
 
 
 // give the bot something to listen for.
-controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
+/* controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
   bot.reply(message,'Hello yourself.');
 });
 
@@ -80,4 +148,4 @@ controller.on('slash_command',function(slashCommand,message) {
 		default:
 			slashCommand.replyPrivate(message,"I don't know how to " + message.command + " yet.");
 	}
-}); 
+}); */
