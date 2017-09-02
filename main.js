ede4fd820c985252b1ae19ab00988216a3362542
Evan <esundwick@gannett.com>
2017-09-02 17:54:07 -0400

    activated fallback code, added a slash handler

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var botkit = require('botkit');
 var debug = require('debug')('botkit:main');
 // var config = require('./config.json');
 
+// read environment for config options
 var bot_options = {
 	clientId: process.env.SLACK_CLIENT_ID,
 	clientSecret: process.env.SLACK_CLIENT_SECRET,
@@ -22,6 +23,7 @@ if (process.env.MONGO_URI) {
     bot_options.json_file_store = __dirname + '/.data/db/'; // store user data in a simple JSON format
 }
 
+// core of the bot
 var controller = botkit.slackbot(bot_options);
 
 controller.startTicking();
@@ -31,6 +33,7 @@ var bot = controller.spawn({
 });
 
 // fix for team errors?
+/*
 bot.api.team.info({}, (err, res) => {
     controller.storage.teams.save({id: res.team.id}, (err) => {
         if (err) {
@@ -38,6 +41,7 @@ bot.api.team.info({}, (err, res) => {
         };
     });
 });
+*/
 
 // Set up an Express-powered webserver to expose oauth and webhook endpoints
 var webserver = require(__dirname + '/components/express_webserver.js')(controller);
@@ -61,8 +65,6 @@ require("fs").readdirSync(normalizedPath).forEach(function(file) {
   require("./skills/" + file)(controller);
 });
 
-
-
 // This captures and evaluates any message sent to the bot as a DM
 // or sent to the bot in the form "@bot message" and passes it to
 // Botkit Studio to evaluate for trigger words and patterns.
@@ -77,7 +79,7 @@ if (process.env.studio_token) {
                 // If you want your bot to respond to every message,
                 // define a 'fallback' script in Botkit Studio
                 // and uncomment the line below.
-                // controller.studio.run(bot, 'fallback', message.user, message.channel);
+              controller.studio.run(bot, 'fallback', message.user, message.channel);
             } else {
                 // set variables here that are needed for EVERY script
                 // use controller.studio.before('script') to set variables specific to a script
@@ -94,18 +96,21 @@ if (process.env.studio_token) {
     console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
 }
 
+controller.on('slash_command',function(slashCommand,message) { 
+  // reply to slash command
+  switch (message.command) {
+      case '/define':
+      // make sure it's actually the bot making the request
+        if (message.token !== process.env.SLACK_VERIFICATION_TOKEN) return; 
+        var defineRequest = message.text;
+        slashCommand.replyPrivate(message, 'You ased to define ' + defineRequest);
+    break;
+    default:
+      slashCommand.replyPrivate(message,"I don't know how to " + message.command + " yet.");
+  }
+});
 
-
-
-function usage_tip() {
-    console.log('~~~~~~~~~~');
-    console.log('Botkit Starter Kit');
-    console.log('Execute your bot application like this:');
-    console.log('clientId=<MY SLACK CLIENT ID> clientSecret=<MY CLIENT SECRET> PORT=3000 studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
-    console.log('Get Slack app credentials here: https://api.slack.com/apps')
-    console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
-    console.log('~~~~~~~~~~');
-}
+/* trying for slash commands */
 
 /* maybe RTM is deprecated...
 bot.startRTM(function(err,bot,payload) {
@@ -136,16 +141,3 @@ setTimeout(function() {
   bot.reply(message,'Hello yourself.');
 });
 
-controller.on('slash_command',function(slashCommand,message) { 
-	// reply to slash command
-	switch (message.command) {
-  		case '/define':
-  		// make sure it's actually the bot making the request
-  			if (message.token !== process.env.SLACK_VERIFICATION_TOKEN) return; 
-  			var defineRequest = message.text;
-  			slashCommand.replyPrivate(message, 'You ased to define ' + defineRequest);
-		break;
-		default:
-			slashCommand.replyPrivate(message,"I don't know how to " + message.command + " yet.");
-	}
-}); */
