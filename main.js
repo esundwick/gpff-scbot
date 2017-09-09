a62c6456f1ca56fd352a9e018b3f590e3cdfd40f
Evan <esundwick@gannett.com>
2017-09-09 10:24:02 -0400

    first stab at slash command

--- main.js
+++ main.js
@@ -89,15 +89,17 @@ if (process.env.studio_token) {
             debug('Botkit Studio: ', err);
         });
     });
-    controller.on('slash_command',function(slashCommand,message) { 
+    controller.on('slash_command', function (slashCommand, message) {
+        // make sure it's actually the bot making the request
+        if (message.token !== process.env.SLACK_VERIFICATION_TOKEN) {
+            return bot.res.send(401, 'Unauthorized');
+        };  
         // reply to slash command
         switch (message.command) {
-        case '/define':
-            // make sure it's actually the bot making the request
-            if (message.token !== process.env.SLACK_VERIFICATION_TOKEN) return; 
-            var defineRequest = message.text;
-            slashCommand.replyPrivate(message, 'You ased to define ' + defineRequest);
-        break;
+            case '/define':
+                var defineRequest = message.text;
+                slashCommand.replyPrivate(message, 'You ased to define ' + defineRequest);
+            break;
         default:
             slashCommand.replyPrivate(message,"I don't know how to " + message.command + " yet.");
         }
@@ -107,44 +109,3 @@ if (process.env.studio_token) {
     console.log('NOTE: Botkit Studio functionality has not been enabled');
     console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
 }
-
-/* maybe RTM is deprecated...
-bot.startRTM(function(err,bot,payload) {
-  if (err) {
-    throw new Error('Could not connect to Slack');
-  };
-});
-
-//prepare the webhook
-controller.setupWebserver(process.env.PORT || 3001, function(err, webserver) {
-    controller.createWebhookEndpoints(webserver, bot, function() {
-        // handle errors...
-        if (err) {
-  			throw new Error('Could not connect to Slack');
-  		};
-    });
-});
-
-// close the RTM for the sake of it in 5 seconds
-setTimeout(function() {
-      bot.closeRTM();
-  }, 5000);
-});*/
-
-
-// give the bot something to listen for.
-/* controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
-  bot.reply(message,'Hello yourself.');
-});
-*/
-
-// fix for team errors?
-
-bot.api.team.info({}, function (err, res) {
-    if (err) return console.error(err);
-    controller.storage.teams.save({id: res.team.id}, (err) => {
-        if (err) {
-            console.error(err);
-        };
-    });
-});
